dcd2773d4cb908fb6a7c95a68eea47a39bbb1645
sua1897 <ltogogo7@gmail.com>
2025-03-06 12:09:52 +0900

    앱 잠금 파일 업로드

---
+++ app/src/main/java/com/naver/appLock/AlarmActivity.kt
@@ -0,0 +1,12 @@
+package com.naver.appLock
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm)
+
+    }
+}
